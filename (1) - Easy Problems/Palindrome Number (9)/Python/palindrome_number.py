f2be0b3f0108126436a5a62661526b66ae6a15cd
Denivic <denivic@protonmail.com>
2022-10-23 20:59:51 +0200

    Removed redundant code from (9)

--- (1) - Easy Problems/Palindrome Number (9)/Python/palindrome_number.py
+++ (1) - Easy Problems/Palindrome Number (9)/Python/palindrome_number.py
@@ -1,10 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         # Convert to string, reverse, and finally check against x
-        return True if str(x)[::-1] == str(x) else False
+        return str(x)[::-1] == str(x)
 
 
 sol = Solution()
+
+# Test cases
 assert sol.isPalindrome(121) is True    # True
 assert sol.isPalindrome(10) is False    # False
 assert sol.isPalindrome(-121) is False  # False
